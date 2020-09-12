4a3b0779b81e84e8a41027831dfe102703ea78b8
Sarat Chandra <me@saratchandra.in>
2020-09-12 08:39:50 +0530

    Add initial scaffolding for the project

---
+++ cmd/zettel/actions.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/urfave/cli/v2"
+)
+
+func initProject(ctx *cli.Context) error {
+	fmt.Println("[*] Initializing new zettel project")
+	return nil
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/hackstream/zettel
+
+go 1.15
+
+require github.com/urfave/cli/v2 v2.2.0
