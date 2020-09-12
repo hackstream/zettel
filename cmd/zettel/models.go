acb440855bcff7e1fc085d5960d0ab4df289741d
Karan Sharma <hello@mrkaran.dev>
2020-09-12 16:59:38 +0530

    fix: cleanup unused models

--- cmd/zettel/models.go
+++ cmd/zettel/models.go
@@ -1,20 +1,7 @@
 package main
 
-import "time"
-
 // Config represents zettel site specific settings.
 type Config struct {
 	SiteName string `toml:"site_name"`
 	Title    string `toml:"title"`
-}
-
-type Post struct {
-	Body     string
-	Metadata Metadata
-	// links    []Link
-}
-
-type Metadata struct {
-	Date  time.Time
-	Title string
 }
