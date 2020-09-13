5d0d076f62bdc30690a6bd55a6f3fa205eaf5944
Sarat Chandra <me@saratchandra.in>
2020-09-13 09:01:51 +0530

    feat: Implement rendering tags & all posts page.

--- cmd/zettel/render.go
+++ cmd/zettel/render.go
@@ -14,52 +14,6 @@ const (
 	defaultDistDir = "dist"
 )
 
-func (hub *Hub) makeDist() error {
-	dirs := []string{
-		defaultDistDir,
-		path.Join(defaultDistDir, "css"),
-		path.Join(defaultDistDir, "images"),
-		path.Join(defaultDistDir, "posts"),
-	}
-
-	for _, d := range dirs {
-		err := createDirectory(d)
-		if err != nil {
-			return err
-		}
-	}
-
-	// Copy css, images folders to dist directory
-	globs := map[string]string{
-		"/templates/layouts/css/*":    path.Join(defaultDistDir, "css"),
-		"/templates/layouts/images/*": path.Join(defaultDistDir, "images"),
-	}
-
-	for g, dir := range globs {
-		files, err := hub.Fs.Glob(g)
-		if err != nil {
-			return err
-		}
-		for _, f := range files {
-			b, err := hub.Fs.Read(f)
-			if err != nil {
-				return err
-			}
-
-			fd, err := os.Create(path.Join(dir, path.Base(f)))
-			if err != nil {
-				return err
-			}
-
-			_, err = fd.Write(b)
-			if err != nil {
-				return err
-			}
-		}
-	}
-	return nil
-}
-
 func (hub *Hub) renderIndex(post pipeline.Post) error {
 	tmplContext := make(map[string]interface{})
 	tmplContext["SiteName"] = hub.Config.SiteName
@@ -109,6 +63,41 @@ func (hub *Hub) renderPost(post pipeline.Post) error {
 		"templates/layouts/footer.tmpl",
 	}
 	err = saveResource("base", tmpls, file, tmplContext, hub.Fs)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (hub *Hub) renderTag(tag string, posts []pipeline.Post, isAllPosts bool) error {
+	// Make links from posts
+	links := []pipeline.Link{}
+	for _, p := range posts {
+		l := pipeline.Link{
+			Slug:  strings.TrimRight(path.Base(p.FilePath), ".md"),
+			Title: p.Meta.Title,
+		}
+		links = append(links, l)
+	}
+	tmplContext := make(map[string]interface{})
+	tmplContext["TagName"] = tag
+	tmplContext["Links"] = links
+	path := filepath.Join(defaultDistDir, "all.html")
+	if !isAllPosts {
+		path = filepath.Join(defaultDistDir, "tags", fmt.Sprintf("%s.html", tag))
+	}
+	file, err := os.Create(path)
+	if err != nil {
+		return err
+	}
+	// render post template
+	tmpls := []string{
+		"templates/layouts/list.tmpl",
+		"templates/layouts/header.tmpl",
+		"templates/layouts/navbar.tmpl",
+		"templates/layouts/footer.tmpl",
+	}
+	err = saveResource("list", tmpls, file, tmplContext, hub.Fs)
 	if err != nil {
 		return err
 	}
